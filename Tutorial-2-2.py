34821f3deee2cf80285a33fb351cc43e0416aad8
Bingqing <chenbq6@gmail.com>
2019-07-30 15:48:21 -0400

    Added GPIO cleanup at KeyBoardInterruption

--- Tutorial-2-2.py
+++ Tutorial-2-2.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,11 +10,19 @@ button = 25
 GPIO.setup(button, GPIO.IN)
 GPIO.setup(red_led, GPIO.OUT)
 
-while True:
-    if GPIO.input(button):
-        GPIO.output(red_led, GPIO.HIGH)
-    else:
-        GPIO.output(red_led, GPIO.LOW)
+def main():
+    while True:
+        if GPIO.input(button):
+            GPIO.output(red_led, GPIO.HIGH)
+        else:
+            GPIO.output(red_led, GPIO.LOW)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit(0)
 
 
 
