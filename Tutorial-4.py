8c7e33d0f2477ca1325a3fc9c7482df99104cd41
chenbq1234 <chenbq6@gmail.com>
2019-08-05 17:46:44 -0400

    Save changes

--- Tutorial-4.py
+++ Tutorial-4.py
@@ -18,22 +18,25 @@ class Device(mqtt.Client):
         self.username = username
         self.password = password
         
-        self.sensors = sensorList
+        self.sensor = sensorList
         self.actuators = actuatorList
-        
+        self.transduces = sensorList + actuatorList
+
         # Set access credential
         self.username_pw_set(username,password) #set username and pass
-        self.tls_set(ca_certs = '/etc/ssl/certs')
+        # self.tls_set(ca_certs = '/etc/ssl/certs')
         
         # Connect to the Broker, i.e. the OpenChirp server
-        self.connect_async(self.host, self.port, self.keepalive)
-        self.loop_start()
+        self.connect(self.host, self.port, self.keepalive)
+        self.loop_forever()
     
     # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
     def on_connect(self, client, userdata, flags, rc):
-        for sensor in self.sensors:
-            self.subscribe("openchirp/device/"+self.username+"/"+ sensor)
+        print("I am here")
+        for transducer in self.transducers:
+            self.subscribe("openchirp/device/"+self.username+"/"+ transducer)
+            print(transducer)
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
@@ -52,7 +55,7 @@ def main():
     
     # Instantiate your own device
     smart_light = Device(username, password, sensorList, actuatorList)
-    
+    '''
     threshold = 2
     while True:
         sensor_reading = smart_light.subscribe
@@ -60,7 +63,7 @@ def main():
             smart_light.publish() # Turn Light On
         else:
             smart_light.publish() # Turn Light Off
-
+    '''
 if __name__ == '__main__':
     try:
         main()
